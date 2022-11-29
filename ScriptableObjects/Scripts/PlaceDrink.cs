e50de5251d273c06408ea0d23af2acb78c0a3e4c
MMCollegeCode <104695429+MMCollegeCode@users.noreply.github.com>
2022-11-29 16:38:46 -0500

    cooking Bar/png edits-Mia

---
+++ ScriptableObjects/Scripts/PlaceDrink.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceDrink : MonoBehaviour
+{
+    public float value = 5.00f;
+    public GameObject cup;
+    private bool on = false;
+    //public Vector2 spawn;
+
+
+    // Start is called before the first frame update
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player" && Input.GetKey(KeyCode.Space)/* && !on*/)
+        {
+            //Instantiate(cup, new Vector2(4.5, -1.0542))
+            cup.SetActive(true);
+
+            //Destroy(cup, 10f);
+            
+            on = true;
+
+        }
+
+
+        else if (col.gameObject.tag == "Player" && Input.GetKey(KeyCode.E))
+        {
+
+            cup.SetActive(false);
+            on = false;
+
+            if (on == false) {
+                KeepScore.score += value;
+            }
+            
+
+        }
+
+
+    }
+
+
+
+    void Start()
+    {
+        cup.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
